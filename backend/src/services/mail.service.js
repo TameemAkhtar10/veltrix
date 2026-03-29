e12fbef3727ccdf8ce5a6d1d266b38a6f8190e33
tameem akhtar <akhtartamimm@gmail.com>
2026-03-29 12:20:33 +0530

    solved nodemailer

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 export const sendEmail = async (to, subject, html, text) => {
     const mailUser = process.env.GOOGLE_USER_EMAIL?.trim();
-   
+
     const mailPass = process.env.GOOGLE_APP_PASSWORD?.replace(/\s+/g, '');
 
     if (!mailUser || !mailPass) {
@@ -10,12 +10,13 @@ export const sendEmail = async (to, subject, html, text) => {
     }
 
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        family: 4,
         auth: {
-            user: mailUser,
-            pass: mailPass,
+            user: process.env.GOOGLE_USER_EMAIL,
+            pass: process.env.GOOGLE_APP_PASSWORD,
         },
     });
 
